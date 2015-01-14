8c828f49d470729db68b781675c7ecf59e578c30
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-01-15 00:25:47 +0900

    add some setting specs

--- spec/dockerfile_spec.rb
+++ spec/dockerfile_spec.rb
@@ -9,4 +9,21 @@ describe 'Sample Images' do
   it 'should exist' do
     expect(@image).not_to be_nil
   end
+
+  it 'should expose the default port' do
+    expect(@image.json['config']['ExposedPorts']).has_key?('22/tcp').to be_true
+  end
+
+  it 'should have CMD' do
+    expect(@image.json['config']['Cmd']).to include('/usr/bin/wc', '--help')
+  end
+
+  it 'should have environmental variable' do
+    expect(@image.json['config']['Env']).to include('TEST=test')
+  end
+
+  it 'should have working directory' do
+    expect(@image.json['config']['WorkingDir']).to eq('/root')
+  end
+
 end
