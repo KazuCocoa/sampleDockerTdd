aedb7cfc7d5c7e56634b033b0ce1d4dd41735e0a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-01-15 00:42:53 +0900

    Fix red and implement dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,9 @@
 FROM ubuntu:14.04
 MAINTAINER kazuaki
+
+EXPOSE 22
+
+ENV TEST=test
+WORKDIR /root
+
+CMD ["/usr/bin/wc", "--help"]
--- spec/dockerfile_spec.rb
+++ spec/dockerfile_spec.rb
@@ -11,19 +11,19 @@ describe 'Sample Images' do
   end
 
   it 'should expose the default port' do
-    expect(@image.json['config']['ExposedPorts']).has_key?('22/tcp').to be_true
+    expect(@image.json['Config']['ExposedPorts'].has_key?('22/tcp')).to be_truthy
   end
 
   it 'should have CMD' do
-    expect(@image.json['config']['Cmd']).to include('/usr/bin/wc', '--help')
+    expect(@image.json['Config']['Cmd']).to include('/usr/bin/wc', '--help')
   end
 
   it 'should have environmental variable' do
-    expect(@image.json['config']['Env']).to include('TEST=test')
+    expect(@image.json['Config']['Env']).to include('TEST=test')
   end
 
   it 'should have working directory' do
-    expect(@image.json['config']['WorkingDir']).to eq('/root')
+    expect(@image.json['Config']['WorkingDir']).to eq('/root')
   end
 
 end
